4da6b16ddb5bd2958d474d32812933bb57f93dfb
george-tr6 <george.tr6@gmail.com>
2018-09-01 16:36:34 -0700

    George T - comboMath - practice31

---
+++ practice31/main.js
@@ -0,0 +1,7 @@
+function cocococombomath(num1, num2, num3, num4, num5){
+    var result1 = num1 * num2;
+    var result2 = result1 + num3;
+    var result3 = result2 / num4;
+    var result4 = result3 - num5;
+    return result4;
+}
